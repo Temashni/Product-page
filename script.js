b250df69fb312ac66d7c4c085be5cb0dc1944056
Artem Mishin <artem.mishinbet98@mail.ru>
2021-05-28 15:50:38 +0400

    product card

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ function unshow(){
   document.getElementById("scroll").style.overflow="auto";
 }
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
+var myswiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
   spaceBetween: 30,
   navigation: {
     nextEl: ".product-card__slider-button__right",
-    prevEl: ".product-card__slider-button",
+    prevEl: ".product-card__slider-button__left",
   },
 });
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".product-card__slider-button__next",
+    prevEl: ".product-card__slider-button__prev",
+  },
+});
+
+function changedViewedImage(event) {
+  console.log(1);
+  const linkToImage = this.event.target.getAttribute("src");
+  document.getElementById("imageOutput").setAttribute("src", linkToImage);
+}
